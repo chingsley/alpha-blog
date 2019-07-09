a315c955e4ba2110e65bc58e37a79bdbd1209d22
chingsley <eneja.kc@gmail.com>
2019-07-09 17:05:12 +0100

    implement admin authorisation for creating categories

--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,6 +2,11 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+    @user = User.create(username: "john", email: "john@example.com", password: "password", admin: true)
+    sign_in_as(@user, "password")
+  end
+
   test "get new category form and create category" do
     get new_category_path
     assert_template 'categories/new'
